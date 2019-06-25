63ac7f00b9717932b6c17d17a7312f96118894e4
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2019-06-25 12:25:18 +0530

    Add files via upload

---
+++ mani_7.java
@@ -0,0 +1,28 @@
+package mani;
+import java.util.Scanner;
+public class mani_7 {
+		public static void main(String[] args) {
+			
+			char ch;
+			int temp;
+			Scanner sc=new Scanner(System.in);
+			ch=sc.next().charAt(0);
+			if(ch>=97&&ch<123)
+			{
+			temp = (int) ch;
+	        temp = temp - 32;
+	        ch = (char) temp;
+	        
+	         System.out.println(ch);
+		}
+			else
+			{
+				temp = (int) ch;
+		        temp = temp + 32;
+		        ch = (char) temp;
+		        System.out.println(ch);
+			}
+
+	
+	}
+}
