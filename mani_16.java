bf4bc6f942f62f8bf94651d5a30d6c8fbbaf9f3b
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2019-06-25 15:34:47 +0530

    Add files via upload

---
+++ mani_16.java
@@ -0,0 +1,19 @@
+package mani;
+import java.util.Scanner;
+public class mani_16 {
+		public static void main(String[] args) {
+			
+			int n,i,j;
+			Scanner s=new Scanner(System.in);
+			n=s.nextInt();
+			for(i=1;i<=n;i++)
+			{
+				for(j=1;j<=i;j++)
+				{
+					System.out.print("*");
+					System.out.print(" ");
+				}
+				System.out.println("");
+			}
+		}
+}
