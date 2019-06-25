7bc0f2210bde8da8d0b7272c1702be02365a186d
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2019-06-25 11:23:20 +0530

    Add files via upload

---
+++ mani_2.java
@@ -0,0 +1,15 @@
+package mani;
+import java.util.Scanner;
+public class mani_2 {
+		public static void main(String[] args) {
+	       int n;
+	       Scanner s=new Scanner(System.in);
+	       n=s.nextInt();
+	       if(n%2==0)
+	    	   System.out.println("even");
+	       else
+	    	   System.out.println("odd");
+		}
+
+	}
+
