fe5e21cd6a14cbe50e8ef005a35a04cf331108bb
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2019-06-25 15:43:31 +0530

    Add files via upload

---
+++ mani_17.java
@@ -0,0 +1,19 @@
+package mani;
+import java.util.Scanner;
+public class mani_17 {
+		public static void main(String[] args) {
+			
+			      int num=0;
+			      int m=0;
+			      Scanner in = new Scanner(System.in);
+			      num = in.nextInt();
+			      while( num != 0 )
+			      {
+			          m=m*10;
+			          m=m+num%10;
+			          num= num/10;
+			      }
+	            System.out.println("reverse="+m);
+		}
+
+}
