f45919960ad915245924a39fc7302449110f4c7a
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2019-06-25 11:32:49 +0530

    Add files via upload

---
+++ mani_3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class mani_3 {
+
+	public static void main(String[] args) {
+		
+        if(args.length==0)
+          {
+             System.out.println("no value");
+           }
+        else
+        {
+        System.out.println(args[0]);
+        System.out.println(",");
+        System.out.println(args[1]);
+	}
+
+}
+}
