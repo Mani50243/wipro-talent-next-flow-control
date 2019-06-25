a06151ebbd2d7b3a0612ff4d873a88626897df97
Mani50243 <52171184+Mani50243@users.noreply.github.com>
2019-06-25 14:43:24 +0530

    Add files via upload

---
+++ mani_11.java
@@ -0,0 +1,20 @@
+package mani;
+
+public class mani_11 {
+
+		public static void main(String[] args) {
+			
+			int i,n;
+			for(i=23;i<57;i++)
+			{
+				if(i%2==0)
+				{
+				System.out.println(i);
+				}
+			}
+
+		}
+
+	
+
+}
